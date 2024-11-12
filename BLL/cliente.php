ef4cce9f5a2fa797f677e607a57e205687ab66f4
Micaella Melo <135082114+MicasMelo@users.noreply.github.com>
2024-11-12 11:35:21 -0300

    BLL

---
+++ BLL/cliente.php
@@ -0,0 +1,31 @@
+<?php
+    namespace BLL;
+    include_once 'C:\xampp\htdocs\gestaosemear\DAL\cliente.php';
+
+    class Cliente{
+        public function Select(){
+            $dalCli = new \DAL\cliente();
+            return $dalCli->Select();
+        }
+
+        public function SelectID(int $id){
+            $dalCli = new \DAL\cliente();
+            return $dalCli->SelectID($id);
+        }
+
+        public function Insert(\MODEL\cliente $cliente){
+            $dalCli = new \DAL\cliente();
+            return $dalCli->Insert($cliente);
+        }
+
+        public function Update(\MODEL\cliente $cliente){
+            $dalCli = new \DAL\cliente();
+            return $dalCli->Update($cliente);
+        }
+
+        public function Delete($id){
+            $dalCli = new \DAL\cliente();
+            return $dalCli->Delete($id);
+        }
+    }
+?>
